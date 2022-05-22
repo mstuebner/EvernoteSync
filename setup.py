ed58a22a25223ca4698d44f9d0d1cc12688b2fbe
MS <mstuebner@gmail.com>
2022-05-22 17:30:37 +0200

    Pylint setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 """
 Configuration file to build a package
 """
+from pathlib import Path
 from setuptools import setup, find_packages
 
 # read the contents of your README file
-from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
