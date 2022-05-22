15d41303efd4829faaa867e2d0f9724831955ffb
MS <mstuebner@gmail.com>
2022-05-22 17:25:27 +0200

    Add long_description to setup.py and change the repository_url again

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
+"""
+Configuration file to build a package
+"""
 from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="Evernote File Importer",
     version="0.3.0",
     author="Matthias Stuebner",
     author_email="mstuebner@gmail.com",
-    url="https://pypi.org/edeediong-resume",
+    url="https://pypi.org/",
     description="An application that monitors a directory structure and automatically imports files into Evernote",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.9",
